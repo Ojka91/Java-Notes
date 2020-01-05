5c35108f48c4e395c12c4d6a609ec613ea473710
Ojka91 <ojkalp@gmail.com>
2020-01-05 13:14:16 +0100

    minor comments

--- src/Exceptions.java
+++ src/Exceptions.java
@@ -1,31 +1,34 @@
 public class Exceptions {
-    public static void main (String [] args) {
-       try {
-           Divide divide = new Divide(10,0);
-       }catch (OperationException ex){
-           System.out.println(ex + ex.getMessage());
-           ex.getMessage();
-       }
+    public static void main(String[] args) {
+        //envolve into trycatch bc it can throws an error
+        try {
+            Divide divide = new Divide(10, 0);
+        } catch (OperationException ex) {
+            System.out.println(ex + ex.getMessage());
+            ex.getMessage();
+        }
     }
-
-    public class Divide{
+}
+     class Divide{
         private int numerator;
         private int denominator;
 
-        public Divide(int numerator, int denominator) throws OperationException{
+        public Divide(int numerator, int denominator) throws OperationException{ //can throws an OperationExc
 
             if(this.denominator == 0){
-                throw new OperationException("Denominator = 0");
+                throw new OperationException("Denominator = 0"); //execute an exception
             }
             this.numerator = numerator;
             this.denominator = denominator;
         }
 
     }
-    public class OperationException extends Exception{
+
+    //creation own exception
+     class OperationException extends Exception{
         public OperationException(String msg){
             super(msg);
         }
 
     }
-}
+
