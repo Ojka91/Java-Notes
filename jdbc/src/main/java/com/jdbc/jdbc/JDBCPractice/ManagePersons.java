3d27ad8c3081dd2644b0be547f14d05314dc0b1c
Ojka91 <ojkalp@gmail.com>
2020-01-10 13:22:22 +0100

    JDBC II Transactionals

--- jdbc/src/main/java/com/jdbc/jdbc/JDBCPractice/ManagePersons.java
+++ jdbc/src/main/java/com/jdbc/jdbc/JDBCPractice/ManagePersons.java
@@ -2,11 +2,13 @@ package com.jdbc.jdbc.JDBCPractice;
 
 import com.jdbc.jdbc.JDBCPractice.domain.Person;
 
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.List;
 
 public class ManagePersons {
     public static void main(String[] args) {
-
+/*
         //Select
         PersonJDBC personJDBC = new PersonJDBC();
         List<Person> persons = personJDBC.select();
@@ -39,5 +41,46 @@ public class ManagePersons {
         Person person2 = new Person();
         person2.setId_person(3);
         personJDBC.delete(person);
+
+ */
+
+
+        //Transactioanl -> We make more than one action with only one connection
+        Connection connection = null;
+        try {
+            connection = Conn.getConnection();
+            if (connection.getAutoCommit()){
+                connection.setAutoCommit(false);  //Do not auto commit. By default is always true
+
+                PersonJDBC personJDBC = new PersonJDBC(connection);
+
+                Person person = new Person();
+                person.setId_person(1);
+                person.setName("ChanegdNameTransactional");
+                person.setPhone("newphine");
+
+                Person person1 = new Person();
+                person1.setName("NewPerson");
+                person1.setSurname("NewSurname");
+
+                personJDBC.update(person);
+                personJDBC.insert(person1);
+                connection.commit(); //We set autoComit false. We must do it manualy
+
+                personJDBC.select();
+
+            }
+            //if try block fails, we roll back all the actions we declared (inserts updates.....)
+        } catch (SQLException e) {
+            e.printStackTrace(System.out);
+            System.out.println("Into rollback");
+            try {
+                connection.rollback();
+            } catch (SQLException ex) {
+                ex.printStackTrace(System.out);
+            }
+
+        }
+
     }
 }
--- jdbc/src/main/java/com/jdbc/jdbc/JDBCPractice/PersonJDBC.java
+++ jdbc/src/main/java/com/jdbc/jdbc/JDBCPractice/PersonJDBC.java
@@ -11,12 +11,25 @@ import java.util.List;
 
 public class PersonJDBC {
 
+    //Transactional connection is used to not open and close a connection each time if we need to do different things
+    //Declare what will be our transactioanlConn (a normal Connection)
+    private Connection transactionalConn;
+
     private static final String SQL_SELECT = "SELECT id_person, name, surname, email, phone FROM person";
     private static final String SQL_INSERT = "INSERT INTO person(name, surname, email, phone) VALUES(?, ?, ?, ?)";
     private static final String SQL_UPDATE = "UPDATE person SET name=?, surname=?, email=?, phone=? WHERE id_person = ?";
     private static final String SQL_DELETE = "DELETE FROM person WHERE id_person = ?";
 
-    public List<Person> select(){
+    //For Transactional we need to add constructors
+    public PersonJDBC(){
+
+    }
+
+    public PersonJDBC(Connection transactionalConn){
+        this.transactionalConn = transactionalConn;
+    }
+
+    public List<Person> select() throws SQLException {
         Connection conn = null;
         PreparedStatement preparedStatement = null;
         ResultSet resultSet = null;
@@ -24,7 +37,8 @@ public class PersonJDBC {
         List<Person> persons = new ArrayList<>();
 
         try {
-            conn = Conn.getConnection();
+            conn = this.transactionalConn != null ? this.transactionalConn : Conn.getConnection(); //if transCon is not open, it will create single connection for this method
+                                                                                    //otherwise wont use this private connection and will use transactional one
             preparedStatement = conn.prepareStatement(SQL_SELECT);
             resultSet = preparedStatement.executeQuery();
             while(resultSet.next()){
@@ -42,10 +56,11 @@ public class PersonJDBC {
 
                 persons.add(person);
             }
-        } catch (SQLException e) {
-            e.printStackTrace(System.out);
+            //We took off catch exception bc testing transactional methods it must propagate the exception so the method transactional catches and process it
         } finally {
+            if(this.transactionalConn == null){ //if we are not using transactional connection ->we are using private one and has to be close
             Conn.close(conn);
+            }
             Conn.close(preparedStatement);
             Conn.close(resultSet);
         }
@@ -53,14 +68,15 @@ public class PersonJDBC {
     }
 
 
-    public int insert(Person person){
+    public int insert(Person person) throws SQLException {
         Connection conn = null;
         PreparedStatement preparedStatement = null;
         ResultSet resultSet = null;
         int rows = 0;
 
         try {
-            conn = Conn.getConnection();
+            conn = this.transactionalConn != null ? this.transactionalConn : Conn.getConnection(); //if transCon is not open, it will create single connection for this method
+            //otherwise wont use this private connection and will use transactional one
             preparedStatement = conn.prepareStatement(SQL_INSERT);
             preparedStatement.setString(1, person.getName());
             preparedStatement.setString(2, person.getSurname());
@@ -71,22 +87,24 @@ public class PersonJDBC {
             System.out.println("Execute query: " + SQL_INSERT);
             rows = preparedStatement.executeUpdate();
             System.out.println("Registers affecteds: " + rows);
-        } catch (SQLException e) {
-            e.printStackTrace(System.out);
-        }finally {
+            //We took off catch exception bc testing transactional methods it must propagate the exception so the method transactional catches and process it
+        } finally {
             Conn.close(preparedStatement);
-            Conn.close(conn);
+            if(this.transactionalConn == null){ //if we are not using transactional connection ->we are using private one and has to be close
+                Conn.close(conn);
+            }
         }
         return rows;
     }
 
-    public int update (Person person){
+    public int update (Person person) throws SQLException {
         Connection conn = null;
         PreparedStatement preparedStatement = null;
         int rows = 0;
 
         try {
-            conn = Conn.getConnection();
+            conn = this.transactionalConn != null ? this.transactionalConn : Conn.getConnection(); //if transCon is not open, it will create single connection for this method
+            //otherwise wont use this private connection and will use transactional one
             System.out.println("executing query " + SQL_UPDATE);
             preparedStatement = conn.prepareStatement(SQL_UPDATE);
             preparedStatement.setString(1, person.getName());
@@ -97,33 +115,35 @@ public class PersonJDBC {
 
             rows = preparedStatement.executeUpdate();
             System.out.println("Registers updateds " + rows);
-
-        } catch (SQLException e) {
-            e.printStackTrace(System.out);
-        }finally {
+            //We took off catch exception bc testing transactional methods it must propagate the exception so the method transactional catches and process it
+        } finally {
             Conn.close(preparedStatement);
-            Conn.close(conn);
+            if(this.transactionalConn == null){ //if we are not using transactional connection ->we are using private one and has to be close
+                Conn.close(conn);
+            }
         }
         return rows;
     }
 
-    public int delete(Person person){
+    public int delete(Person person) throws SQLException {
         Connection connection = null;
         PreparedStatement preparedStatement = null;
         int rows = 0;
 
         try {
-            connection = Conn.getConnection();
+            connection = this.transactionalConn != null ? this.transactionalConn : Conn.getConnection(); //if transCon is not open, it will create single connection for this method
+            //otherwise wont use this private connection and will use transactional one
             System.out.println("Executing query " + SQL_DELETE);
             preparedStatement = connection.prepareStatement(SQL_DELETE);
             preparedStatement.setInt(1, person.getId_person());
             rows = preparedStatement.executeUpdate();
             System.out.println("Registers eliminated " + rows);
-        } catch (SQLException e) {
-            e.printStackTrace(System.out);
+            //We took off catch exception bc testing transactional methods it must propagate the exception so the method transactional catches and process it
         }finally {
             Conn.close(preparedStatement);
-            Conn.close(connection);
+            if(this.transactionalConn == null){ //if we are not using transactional connection ->we are using private one and has to be close
+                Conn.close(connection);
+            }
         }
         return rows;
     }
